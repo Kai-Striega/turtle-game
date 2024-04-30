e559c0310efad5e1a2006004aa64e7fd794c1a6c
Kai Striega <me@kaistriega.com>
2024-04-30 15:33:33 +1000

    Make BoundingBox contains helper methods private

--- turtle_game/bounding_box.py
+++ turtle_game/bounding_box.py
@@ -15,12 +15,12 @@ class BoundingBox:
         self.bottom_bound = bottom_bound
         self.top_bound = top_bound
 
-    def is_in_x_bound(self, x: int | float) -> bool:
-        return self.left_bound < x < self.right_bound
-
-    def is_in_y_bound(self, y: int | float) -> bool:
-        return self.bottom_bound < y < self.top_bound
-
     def __contains__(self, item: HasPosition) -> bool:
         x, y = item.position()
-        return self.is_in_x_bound(x) and self.is_in_y_bound(y)
+        return self._is_in_x_bound(x) and self._is_in_y_bound(y)
+
+    def _is_in_x_bound(self, x: int | float) -> bool:
+        return self.left_bound < x < self.right_bound
+
+    def _is_in_y_bound(self, y: int | float) -> bool:
+        return self.bottom_bound < y < self.top_bound
